13bc7a4431beeb05f0da333bbbe51c7ec104d750
Katumbela <ja3328173@gmail.com>
2024-06-30 23:42:00 +0100

    edit model file

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -14,10 +13,15 @@ df = pd.DataFrame(data)
  
 X = df[['area', 'bedrooms', 'bathrooms']]
 y = df['price']
+
  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
- 
+
+
 model = LinearRegression()
-model.fit(X_train, y_train) 
+model.fit(X_train, y_train)
+
 
 joblib.dump(model, 'house_price_model.pkl')
+
+print("Modelo treinado e salvo como 'house_price_model.pkl'")
